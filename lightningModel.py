caf9b5da640b330b12c5aad067fa717509ca640b
stmhap <141840169+stmhap@users.noreply.github.com>
2023-10-05 17:34:41 +0530

    bug fix

--- lightningModel.py
+++ lightningModel.py
@@ -23,21 +23,23 @@ class CustomUnet(pl.LightningModule):
 
    
     def training_step(self, batch, batch_idx):
-        input_imgs, target_masks = batch
-        pred_mask_prob =  self.model(input_imgs) 
-        target_masks = target_masks*255 #PIL Image open divides all values by 255
 
-             
-        # Reshape the predicted output tensor to [batch_size * height * width, num_classes]
-        predicted_output = pred_mask_prob.permute(0, 2, 3, 1).contiguous().view(-1, 3)
 
-        # cross entropy works based on indices, so convert classes 1,2,3 to 0,1,2
-        target_categories = target_masks - 1
+        if(self.loss == 'CE'): #Cross Entropy loss 
 
-        # Squeeze the singleton dimension from the target tensor and convert to long tensor
-        target_labels = torch.squeeze(target_categories, dim=1).view(-1).long()
+            input_imgs, target_masks = batch
+            pred_mask_prob =  self.model(input_imgs) 
+            target_masks = target_masks*255 #PIL Image open divides all values by 255     
+            
+            # Reshape the predicted output tensor to [batch_size * height * width, num_classes]
+            predicted_output = pred_mask_prob.permute(0, 2, 3, 1).contiguous().view(-1, 3)
 
-        if(self.loss == 'CE'): #Cross Entropy loss
+            # cross entropy works based on indices, so convert classes 1,2,3 to 0,1,2
+            target_categories = target_masks - 1
+
+            # Squeeze the singleton dimension from the target tensor and convert to long tensor
+            target_labels = torch.squeeze(target_categories, dim=1).view(-1).long()
+
             # Create the CrossEntropyLoss criterion
             criterion = torch.nn.CrossEntropyLoss()
 
@@ -54,9 +56,11 @@ class CustomUnet(pl.LightningModule):
             # From the probability of predictions for 3 classes, construct the actual prediction of values - 1,2,3
             # max_channels = torch.argmax(pred_mask_prob, dim=1) + 1
             # result_tensor_predicted = max_channels.unsqueeze(1)
+            input_imgs, target_masks = batch
+            pred_mask_prob =  self.model(input_imgs)            
 
             #print(result_tensor_predicted.shape)
-            loss = dice_loss_fn(predicted_output, target_labels, n_classes=3)
+            loss = dice_loss_fn(pred_mask_prob, target_masks.squeeze(1), n_classes=3)
             
             self.train_losses.append(loss.item())
             
